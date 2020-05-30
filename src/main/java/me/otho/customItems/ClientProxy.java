fc9bf5cf66e56dfb3a59613637a969a7c73c3d5c
Rikka0w0 <929513338@qq.com>
2020-05-30 19:38:13 +1000

    Removed the function of auto adding generated resource pack

--- src/main/java/me/otho/customItems/ClientProxy.java
+++ src/main/java/me/otho/customItems/ClientProxy.java
@@ -1,15 +1,5 @@
 package me.otho.customItems;
 
-import java.util.List;
-
-import me.otho.customItems.utility.LogHelper;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.ClientResourcePackInfo;
-import net.minecraft.resources.IResourceManager;
-import net.minecraft.resources.IResourcePack;
-import net.minecraft.resources.ResourcePackList;
-import net.minecraft.resources.SimpleReloadableResourceManager;
-
 public class ClientProxy extends CommonProxy {
 	@Override
 	public void runClientDataGenerators() {
@@ -20,22 +10,21 @@ public class ClientProxy extends CommonProxy {
 		// Run client data generators
 		CustomDataGenerator.runClient();
 
-		String resPackGenName = "file/" + ResourcePaths.generated;
-		List<String> resPackNames = Minecraft.getInstance().gameSettings.resourcePacks;
-		if (!resPackNames.contains(resPackGenName)) {
-			resPackNames.add(resPackGenName);
-			
-			ResourcePackList<ClientResourcePackInfo> resPackList = Minecraft.getInstance().getResourcePackList();
-			Minecraft.getInstance().gameSettings.fillResourcePackList(resPackList);
-			
-			IResourcePack resPack = resPackList.getPackInfo(resPackGenName).getResourcePack();
-			IResourceManager manager = Minecraft.getInstance().getResourceManager();
-			if (manager instanceof SimpleReloadableResourceManager) {
-				((SimpleReloadableResourceManager) manager).addResourcePack(resPack);
-				LogHelper.info("Add customitems_generated to the resource pack list");
-			} else {
-				LogHelper.warn("Unable to add customitems_generated to the resource pack list");
-			}
-		}
+//		ResourcePackList<ClientResourcePackInfo> resPackList = Minecraft.getInstance().getResourcePackList();
+//		String resPackGenName = "file/" + ResourcePaths.generated;
+//		ClientResourcePackInfo resPackInfo = resPackList.getPackInfo(resPackGenName);
+//		if (resPackInfo == null) {
+//			Minecraft.getInstance().gameSettings.resourcePacks.add(resPackGenName);
+//			Minecraft.getInstance().gameSettings.fillResourcePackList(resPackList);
+//			
+//			IResourcePack resPack = resPackList.getPackInfo(resPackGenName).getResourcePack();
+//			IResourceManager manager = Minecraft.getInstance().getResourceManager();
+//			if (manager instanceof SimpleReloadableResourceManager) {
+//				((SimpleReloadableResourceManager) manager).addResourcePack(resPack);
+//				LogHelper.info("Add customitems_generated to the resource pack list");
+//			} else {
+//				LogHelper.warn("Unable to add customitems_generated to the resource pack list");
+//			}
+//		}
 	}
 }
